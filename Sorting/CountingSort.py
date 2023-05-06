9d463cbf54287470129a98294798d606fe342502
Jiaming Pei <pjming00006@gmail.com>
2023-05-06 16:11:46 -0700

    added quicksort and counting sort

---
+++ Sorting/CountingSort.py
@@ -0,0 +1,40 @@
+import pdb
+
+class Solution:
+
+    def find_solution(self, nums):
+        """
+        If input array is only between 0-9:
+        - Find the cumulative counts from 0-9 and store in an array
+        - Traversing form the end of input, insert value into the correct position
+        - Position index minus 1
+        """
+        if len(nums) <= 1:
+            return nums
+
+        cnts = [0] * 10
+        for n in nums:
+            cnts[n] += 1
+
+        for i in range(1,len(cnts)):
+            cnts[i] = cnts[i-1] + cnts[i]
+
+        res = [0] * len(nums)
+        for i in range(len(nums)-1, -1, -1):
+            res[cnts[nums[i]] - 1] = nums[i]
+            cnts[nums[i]] -= 1
+        return res
+
+
+def main():
+    s = Solution()
+    in_arr = [7,5,3,0,1,3,7,8]
+
+    print(in_arr)
+    result1 = s.find_solution(in_arr)
+
+    print(result1)
+
+
+if __name__ == "__main__":
+    main()
--- Sorting/QuickSort.py
+++ Sorting/QuickSort.py
@@ -13,10 +13,16 @@ class Solution:
         Recursively call the function on left side and right side
         """
         if len(nums) <= 1:
+            print(f"Current array: {nums}, returned")
             return nums
-        print(f"Current array: {nums}")
-        pivot_ind = 0
+
         l, r = 1, len(nums) - 1
+        mid = r // 2
+        nums[0], nums[mid] = nums[mid], nums[0]
+
+        pivot_ind = 0
+
+        print(f"Current array: {nums}, current pivot: {nums[pivot_ind]}")
 
         while l <= r:
             while l < len(nums) and nums[l] <= nums[pivot_ind]:
@@ -28,17 +34,24 @@ class Solution:
                 nums[l], nums[r] = nums[r], nums[l]
             else:
                 nums[pivot_ind], nums[l-1] = nums[l-1], nums[pivot_ind]
+        print(f"Array after: {nums}")
 
-        left_arr = self.find_solution(nums[:l-1])
-        right_arr = self.find_solution(nums[l:])
-
+        if len(nums[:l-1]) < len(nums[l:]):
+            print("left array shorter.")
+            left_arr = self.find_solution(nums[:l-1])
+            right_arr = self.find_solution(nums[l:])
+        else:
+            print("right array shorter.")
+            right_arr = self.find_solution(nums[l:])
+            left_arr = self.find_solution(nums[:l - 1])
         res = left_arr + [nums[l-1]] + right_arr
         return res
 
 
 def main():
     s = Solution()
-    in_arr = [14,22,12,18,19,11,8,13,9]
+    # in_arr = [14,22,12,18,19,11,8,13,9]
+    in_arr = [1,2,3,4,5,6,7,8,9,10]
 
     print(in_arr)
     result1 = s.find_solution(in_arr)
