7e82745d10f9d1059777e736ca54ca85cd80ac1d
Jiaming Pei <pjming00006@gmail.com>
2023-05-13 16:10:03 -0700

    added space saving solution for LC 238

--- Array/LC_238_MID_ProductOfArrayExceptSelf.py
+++ Array/LC_238_MID_ProductOfArrayExceptSelf.py
@@ -16,3 +16,17 @@ class Solution:
         for i in range(len(nums)):
             res[i] = left_p[i] * right_p[i]
         return res
+
+    def find_solution_space(self, nums):
+        # This approach users O(1) space(excluding the result array)
+        left_p, right_p = [1] * len(nums), [1] * len(nums)
+        for i in range(1, len(nums)):
+            left_p[i] = left_p[i - 1] * nums[i - 1]
+        print(left_p)
+
+        R = 1
+        for i in reversed(range(len(nums))):
+            print(i)
+            left_p[i] = left_p[i] * R
+            R = R * nums[i]
+        return left_p
