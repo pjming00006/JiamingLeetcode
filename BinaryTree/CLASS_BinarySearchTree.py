358136359139d6358d8e0570e64817fa45d75b8e
Jiaming Pei <pjming00006@gmail.com>
2023-06-17 16:01:19 -0700

    developed BST with capabilities to construct balanced tree and remove node

--- BinaryTree/CLASS_BinarySearchTree.py
+++ BinaryTree/CLASS_BinarySearchTree.py
@@ -41,55 +41,63 @@ class BinarySearchTree:
                     else:
                         root = root.right
 
+    def remove(self, val, current=None, parent=None):
+        if not current:
+            current = self.root
 
+        while current:
+            if val < current.val:
+                parent = current
+                current = current.left
+            elif val > current.val:
+                parent = current
+                current = current.right
+            else:
+                # Found the target node.
+                # If node has left and right, find the min value from right and replace here
+                # This branch works when the root node is the target value since it does not use the parent node
+                if current.left and current.right:
+                    next_min = self.find_extreme(current.right, is_max=False)
+                    current.val = next_min
+                    self.remove(val=next_min, current=current.right, parent=current)
 
-    def remove(self, val):
-        # 3 scenarios: node at leaf, node has 1 child, node has 2 children
-
-        parent = self.root
-        target_node = self.find_value(val)
-
-        if target_node:
-            while True:
-                if parent == target_node:
-                    print("111")
-                else:
-                    if val < parent.val:
-                        if parent.left == target_node:
-                            print("Found!")
-                            is_left = True
-                            break
-                        else:
-                            parent = parent.left
+                # If parent is None, which means target node is the root node
+                elif not parent:
+                    if current.left:
+                        self.root = current.left
+                    elif current.right:
+                        self.root = current.right
                     else:
-                        if parent.right == target_node:
-                            print("Found!")
-                            is_left = False
-                            break
-                        else:
-                            parent = parent.right
-            # Type 1: node is leaf
-            if not target_node.left and not target_node.right:
-                if is_left:
-                    parent.left = None
-                else:
-                    parent.right = None
+                        self.root = None
 
-            # Type 2: node has 1 child
+                # This branch and the one below do not work when parent is None, so we take care of it above
+                elif current.left or current.right:
+                    if parent.left == current:
+                        parent.left = current.left if current.left else current.right
+                    else:
+                        parent.right = current.left if current.left else current.right
+
+                elif not current.left and not current.right:
+                    if parent.left == current:
+                        parent.left = None
+                    else:
+                        parent.right = None
+
+                self.size -= 1
+                return
 
 
     def find_value(self, val):
         root = self.root
 
         while root:
-            if root.val == val:
-                print("Found the target value.")
-                return root
+            if val < root.val:
+                root = root.left
+            elif val > root.val:
+                root = root.right
             else:
-                if val < root.val:
-                    root = root.left
-                else:
-                    root = root.right
+                print("Target value found")
+                return root
 
         print("Target value not found")
         return None
@@ -98,6 +106,20 @@ class BinarySearchTree:
         for item in in_arr:
             self.insert(item)
         print("Constructed the tree.")
+
+    def construct_balanced(self, in_arr):
+        def helper(in_arr):
+            if not in_arr:
+                return
+            l, r = 0, len(in_arr) - 1
+            while l <= r:
+                mid = (l + r) // 2
+                self.insert(in_arr[mid])
+                helper(in_arr[l:mid])
+                helper((in_arr[mid+1:]))
+                return
+        helper(in_arr)
+
 
     # Return the maximum or the minimum node of the tree
     def find_extreme(self, root, is_max=True):
@@ -108,12 +130,12 @@ class BinarySearchTree:
             while root and root.left:
                 root = root.left
 
-        return root
+        return root.val
 
     def print_by_level(self):
         if self.size == 0:
             return
-
+        print("Printing tree...")
         root = self.root
         in_arr = [root]
         while len(in_arr) != 0:
@@ -131,6 +153,11 @@ class BinarySearchTree:
 if __name__ == "__main__":
     tree = BinarySearchTree()
 
-    tree.construct([10, 5, 15, 3, 7, 12, 17])
-    tree.print_by_level()
+    # tree.construct([10, 5, 15, 3, 7, 12, 17])
+    # tree.print_by_level()
+    # tree.remove(10)
+    # tree.print_by_level()
 
+    l1 = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29]
+    tree.construct_balanced(l1)
+    tree.print_by_level()
