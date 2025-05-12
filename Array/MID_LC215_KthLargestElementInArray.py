f4e321ffffb68190d95887898bca2d8875fa6093
pjming00006 <pjming00006@gmail.com>
2025-05-12 15:28:03 -0700

    lc215 another solution

--- Array/MID_LC215_KthLargestElementInArray.py
+++ Array/MID_LC215_KthLargestElementInArray.py
@@ -36,3 +36,25 @@ class Solution:
                 heapq.heappop(q)
 
         return q[0]
+
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        def quick_select(nums, k):
+            pivot = random.choice(nums)
+            left, mid, right = [], [], []
+
+            for n in nums:
+                if n > pivot:
+                    left.append(n)
+                elif n == pivot:
+                    mid.append(n)
+                else:
+                    right.append(n)
+
+            # If left has more than k elements, the kth must be within left
+            if len(left) >= k:
+                return quick_select(left, k)
+            elif len(left) + len(mid) < k:
+                return quick_select(right, k - (len(left) + len(mid)))
+            return pivot
+
+        return quick_select(nums, k)
